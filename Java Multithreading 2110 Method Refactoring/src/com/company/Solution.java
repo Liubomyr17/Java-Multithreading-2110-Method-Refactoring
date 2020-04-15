40bcf01cddf629f0eeaa555d869ef5876e7e4fc2
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-04-15 22:24:14 +0300

    Add files via upload

---
+++ Java Multithreading 2110 Method Refactoring/src/com/company/Solution.java
@@ -0,0 +1,59 @@
+package com.company;
+
+/*
+
+2110 Method Refactoring
+Refactor the writeZipEntriesToFile method according to java7 try-with-resources.
+
+Requirements:
+1. The writeZipEntriesToFile method must be declared with the public access modifier.
+2. The writeZipEntriesToFile method must be static.
+3. The return type of the writeZipEntriesToFile method must be void.
+4. The writeZipEntriesToFile method should correctly use try-with-resources. The program should not be commented out code.
+
+
+ */
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+
+public class Solution {
+    public static void writeZipEntriesToFile(String zipFileName, String outputFileName) {
+        Charset charset = StandardCharsets.UTF_8;
+        Path outputFilePath = Paths.get(outputFileName);
+
+        try {
+            try (
+                    ZipFile zip = new ZipFile(zipFileName);
+                    BufferedWriter writer = Files.newBufferedWriter(outputFilePath, charset);
+            ) {
+
+                String newLine = System.getProperty("line.separator");
+                for (Enumeration entries = zip.entries(); entries.hasMoreElements(); ) {
+                    // Берем имя файла из архива и записываем его в результирующий файл
+                    // Get the entry name and write it to the output file
+                    String zipEntryName = ((ZipEntry) entries.nextElement()).getName() + newLine;
+                    writer.write(zipEntryName, 0, zipEntryName.length());
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
